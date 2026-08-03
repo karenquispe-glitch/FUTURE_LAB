d147d3b57922b80277dc3cab2bc44eac2a3b4f55
karenquispe-glitch <karen.quispe@esam.edu.bo>
2026-08-03 09:50:47 -0400

    Carga inicial del proyecto FutureLab Medical Simulator
    codigo

---
+++ FutureLab_MedicalSimulator/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject clinicalCasesPanel;
+
+    public void OpenClinicalCases()
+    {
+        mainMenuPanel.SetActive(false);
+        clinicalCasesPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        clinicalCasesPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void StartSimulation()
+    {
+        SceneManager.LoadScene("Hospital_Base");
+    }
+}
